6a9ed4444ce5e0a6a6777590d33032a33dc20fc3
Edmar-15 <0323-3644@lspu.edu.ph>
2026-04-21 22:47:02 +0800

    fix profile to user models, add profile navigation

--- landing/urls.py
+++ landing/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 from account.views import home
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('/home', home, name='home'),
-]
+    path('home/', home, name='home'),
+    path('profile/', views.profile, name='profile'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- landing/views.py
+++ landing/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from account.models import UserProfile
 
 # Create your views here.
 def welcome(request):
     return render(request,'welcome.html')
+
+@login_required
+def profile(request):
+    userProfile = request.user.userprofile
+    return render(request, 'profile.html', {
+        'profile': userProfile,
+    })
