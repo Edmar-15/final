05f46a08da0e6946fec895fc74073b1970c740ce
Kevin-Webdeve <0323-4526@lspu.edu.ph>
2026-04-22 22:35:27 +0800

    add a design to welcome about,contact, login and sign up

--- landing/urls.py
+++ landing/urls.py
@@ -3,5 +3,7 @@ from . import views
 from account.views import home
 
 urlpatterns = [
-    path('/home', home, name='home'),
+    path('home', home, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
 ]
