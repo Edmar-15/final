4745ba27621bfb3d635e56efdc605bae5a148392
Kevin-Webdeve <0323-4526@lspu.edu.ph>
2026-04-25 22:11:38 +0800

    update forms.py

--- account/forms.py
+++ account/forms.py
@@ -9,29 +9,13 @@ ALLOWED_DOMAINS = [
 ID_PATTERN = r"^\d{4}-\d{4}$"
 
 class RegisterForm(forms.ModelForm):
-    password1 = forms.CharField(widget=forms.PasswordInput)
-    password2 = forms.CharField(widget=forms.PasswordInput)
-
+    password = forms.CharField(widget=forms.PasswordInput)
 
     class Meta:
         model = User
         fields = [
-            'email', 
+            'email', 'password'
         ]
-
-    def clean(self):
-        cleaned_data = super().clean()
-        p1 = cleaned_data.get("password1")
-        p2 = cleaned_data.get("password2")
-
-        if p1 != p2:
-            raise forms.ValidationError("Passwords do not match")
-
-        if len(p1) < 8:
-            raise forms.ValidationError("Password must be at least 8 characters")
-
-        return cleaned_data
-
 
     def clean_email(self):
         email = self.cleaned_data.get('email')
@@ -54,7 +38,7 @@ class RegisterForm(forms.ModelForm):
     
     def save(self, commit=True):
         email = self.cleaned_data['email']
-        password = self.cleaned_data['password1']
+        password = self.cleaned_data['password']
         user = User.objects.create_user(email=email, password=password)
         return user
     
@@ -66,8 +50,3 @@ class AccountCompletion(forms.ModelForm):
             'last_name',
             'profile_pic',
         ]
-        widgets = {
-            'first_name': forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'Enter your first name'}),
-            'last_name': forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'Enter your last name'}),
-            'profile_pic': forms.FileInput(attrs={'class': 'form-file', 'accept': 'image/*'}),
-        }
