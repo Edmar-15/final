4b7435a600254c470ea9deffb55aedd8ba2212f3
Kevin-Webdeve <0323-4526@lspu.edu.ph>
2026-04-25 22:26:09 +0800

    fix forms.py

--- account/forms.py
+++ account/forms.py
@@ -9,13 +9,28 @@ ALLOWED_DOMAINS = [
 ID_PATTERN = r"^\d{4}-\d{4}$"
 
 class RegisterForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
+    password1 = forms.CharField(widget=forms.PasswordInput)
+    password2 = forms.CharField(widget=forms.PasswordInput)
 
     class Meta:
         model = User
         fields = [
-            'email', 'password'
+            'email',
         ]
+
+    def clean(self):
+        cleaned_data = super().clean()
+        p1 = cleaned_data.get("password1")
+        p2 = cleaned_data.get("password2")
+
+        if p1 != p2:
+            raise forms.ValidationError("Passwords do not match")
+
+        if len(p1) < 8:
+            raise forms.ValidationError("Password must be at least 8 characters")
+
+        return cleaned_data 
+
 
     def clean_email(self):
         email = self.cleaned_data.get('email')
@@ -38,7 +53,7 @@ class RegisterForm(forms.ModelForm):
     
     def save(self, commit=True):
         email = self.cleaned_data['email']
-        password = self.cleaned_data['password']
+        password = self.cleaned_data['password1']
         user = User.objects.create_user(email=email, password=password)
         return user
     
