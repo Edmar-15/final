2755ba47e80a54850d4d13f141e7ab25b3107566
Edmar-15 <0323-3644@lspu.edu.ph>
2026-04-27 22:27:43 +0800

    fix the folder structure and remove the redundancy

--- final/account/templatetags/usernames_filter.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def mask_username(username):
-    if len(username) <= 4:
-        return "*" * len(username)
-    return username[:2] + "*" * (len(username) - 4) + username[-2:]
